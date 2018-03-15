63f44e1d1257d29155ac7a0fad979397fe477a8b
Joshua Crum <Josh@Joshuas-MacBook-Air.local>
2018-03-15 13:15:05 -0400

    Added comments and signal handlers.

--- server.c
+++ server.c
@@ -1,18 +1,42 @@
-#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <errno.h>
 #include <netinet/in.h>
+#include <pthread.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
 #include <unistd.h>
-#include <arpa/inet.h>
-#include <pthread.h>
-#include <errno.h>
 
 
+
+/* Global client socket to close in signal handler. */
+//FIX ME: this will have to be an array of all client connections later.
+int clientsocket;
+
+
+/* ^c signal handler. */
+void sig_handler(int sigNum)
+{
+	printf("\nClosing client sockets...\n");
+    close(clientsocket);
+	exit(0);
+}
+
+
+/**
+ * Receiving function that takes an arbitrary value as a parameter
+ * and casts it to an integer to receive on that file descriptor.
+ * 
+ * @param *arg a value of any type, recommended integer.
+ * @return void * adaptable to return different types.
+ */
 void *recv_msg(void *arg)
 {
     while (1)
     {
+        /* Cast the parameter to an int. */
         int clientsocket = *((int *)arg);
         char line[5000];
         recv(clientsocket, line, 5000, 0);
@@ -22,8 +46,14 @@ void *recv_msg(void *arg)
     return arg;
 }
 
+
+/**
+ *
+ */
 int main(int argc, char **argv)
 {
+    signal(SIGINT, sig_handler);
+
     int port, status;
     char cp_num[16];
     pthread_t recv;
@@ -31,7 +61,6 @@ int main(int argc, char **argv)
     printf("Enter a port number: ");
     fgets(cp_num, 16, stdin);
     port = atoi(cp_num);
-    printf("Port number :%d:\n", port);
 
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -44,7 +73,7 @@ int main(int argc, char **argv)
     listen(sockfd, 10);
 
     socklen_t len = sizeof(clientaddr);
-    int clientsocket = accept(sockfd, (struct sockaddr *)&clientaddr, &len);
+    clientsocket = accept(sockfd, (struct sockaddr *)&clientaddr, &len);
 
     if ((status = pthread_create(&recv, NULL, recv_msg, &clientsocket)) != 0)
     {
@@ -60,5 +89,5 @@ int main(int argc, char **argv)
         send(clientsocket, line, strlen(line) + 1, 0);
     }
 
-    close(clientsocket);
+    return 0;
 }
