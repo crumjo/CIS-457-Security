fae3682ef0625f86d98e572956d48f5f274d51a1
Joshua Crum <Josh@Joshuas-MacBook-Air.local>
2018-03-19 00:19:36 -0400

    Handles multiple clients and supports broadcast for server.

--- server.c
+++ server.c
@@ -9,30 +9,21 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-
+#define STDIN 0
 
 /* Global client socket to close in signal handler. */
 //FIX ME: this will have to be an array of all client connections later.
 int clientsocket;
 
-
 /* ^c signal handler. */
 void sig_handler(int sigNum)
 {
-	printf("\nClosing client sockets...\n");
+    printf("\nClosing client fds...\n");
     close(clientsocket);
-	exit(0);
+    exit(0);
 }
 
-
-/**
- * Receiving function that takes an arbitrary value as a parameter
- * and casts it to an integer to receive on that file descriptor.
- * 
- * @param *arg a value of any type, recommended integer.
- * @return void * adaptable to return different types.
- */
-void *recv_msg(void *arg)
+void *connection_handler(void *arg)
 {
     while (1)
     {
@@ -46,7 +37,6 @@ void *recv_msg(void *arg)
     return arg;
 }
 
-
 /**
  *
  */
@@ -54,40 +44,115 @@ int main(int argc, char **argv)
 {
     signal(SIGINT, sig_handler);
 
-    int port, status;
     char cp_num[16];
-    pthread_t recv;
+    char welcome[] = "Welcome to the chat server!\n";
+    int port, sockfd, new_socket;
+    int max_clients = 10;
+    int sockets[max_clients];
+    fd_set fds;
 
     printf("Enter a port number: ");
     fgets(cp_num, 16, stdin);
     port = atoi(cp_num);
 
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-    struct sockaddr_in serveraddr, clientaddr;
-    serveraddr.sin_family = AF_INET;
-    serveraddr.sin_port = htons(9876);
-    serveraddr.sin_addr.s_addr = INADDR_ANY;
-
-    bind(sockfd, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
-    listen(sockfd, 10);
-
-    socklen_t len = sizeof(clientaddr);
-    clientsocket = accept(sockfd, (struct sockaddr *)&clientaddr, &len);
-
-    if ((status = pthread_create(&recv, NULL, recv_msg, &clientsocket)) != 0)
+    for (int i = 0; i < max_clients; i++)
     {
-        fprintf(stderr, "Thread create error %d: %s\n", status, strerror(status));
-        exit(1);
+        sockets[i] = 0;
     }
+
+    /* Create the socket. */
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("Socket creation failed.\n");
+        return 1;
+    }
+
+    /* Socket options for multiple connections. */
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0)
+    {
+        perror("Set socket options failed.\n");
+        return 1;
+    }
+
+    struct sockaddr_in serveraddress, clientaddr;
+    serveraddress.sin_family = AF_INET;
+    serveraddress.sin_port = htons(port);
+    serveraddress.sin_addr.s_addr = INADDR_ANY;
+
+    /* Bind to user entered port. */
+    bind(sockfd, (struct sockaddr *)&serveraddress, sizeof(serveraddress));
+
+    /* Listen for max pending connections. */
+    listen(sockfd, max_clients);
+
+    socklen_t len = sizeof(serveraddress);
+
+    FD_ZERO(&fds);
+    FD_SET(sockfd, &fds);
+    FD_SET(STDIN, &fds);
 
     while (1)
     {
-        printf("Enter a line: ");
-        char line[5000];
-        fgets(line, 5000, stdin);
-        send(clientsocket, line, strlen(line) + 1, 0);
-    }
+        fd_set tmp_set = fds;
+        len = sizeof(clientaddr);
 
+        select(FD_SETSIZE, &tmp_set, NULL, NULL, NULL);
+
+        for (int i = 0; i < FD_SETSIZE; i++)
+        {
+            /* Check for connections. */
+            if (FD_ISSET(i, &tmp_set))
+            {
+                if (i == sockfd)
+                {
+                    if ((new_socket = accept(sockfd, (struct sockaddr *)&clientaddr, &len)) < 0)
+                    {
+                        perror("New client accept failed.\n");
+                        return 1;
+                    }
+
+                    FD_SET(new_socket, &fds);
+                    for (int j = 0; j < max_clients; j++)
+                    {
+                        if (sockets[j] == 0)
+                        {
+                            sockets[j] = new_socket;
+                            break;
+                        }
+                    }
+
+                    printf("New connection, sock fd:%d \t ip: %s \t port: %d\n",
+                           new_socket, inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
+
+                    if ((send(new_socket, welcome, strlen(welcome), 0)) != strlen(welcome))
+                    {
+                        perror("Send failed.\n");
+                        return 1;
+                    }
+                }
+                else if (i == STDIN)
+                {
+                    char input[1024];
+                    fgets(input, 1024, stdin);
+                    printf("Stdin :%s:\n", input);
+                    for (int k = 0; k < max_clients; k++)
+                    {
+                        if (sockets[k] == 0)
+                            continue;
+                        send(sockets[k], input, strlen(input) + 1, 0);
+                    }
+                }
+                else
+                {
+                    char line[5000];
+                    recv(i, line, 5000, 0);
+                    printf("Got from client: %s\n", line);
+                    send(i, line, strlen(line) + 1, 0);
+                    // close(i);
+                    // FD_CLR(i, &fds);
+                }
+            }
+        }
+    }
     return 0;
 }
